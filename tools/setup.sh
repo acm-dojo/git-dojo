7d615898e1f59339ada70d65ed51b2d88876e0af
RayZh <rayzhangshanghai@hotmail.com>
2025-09-29 19:02:21 +0800

    refactor: move tooling section to self-contained repo

--- tools/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SCRIPT_FULL_PATH="$(readlink -f "$0")"
-SCRIPT_DIR="$(dirname "$SCRIPT_FULL_PATH")"
-
-docker build -t dojo-gpp -f Dockerfile.dojo-gpp .
-export $PATH="$SCRIPT_DIR/scripts:$PATH"
